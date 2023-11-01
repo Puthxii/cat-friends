da0df727144083d3fa77aad27c9fded0d08b9de7
Teenapat Rattanawong <puthxii@gmail.com>
2023-11-01 13:29:53 +0700

    Initial

---
+++ src/Card.js
@@ -0,0 +1,13 @@
+function Card({name, email, id}) {
+    return (
+        <div className='bg-dark-blue dib br3 pa3 ma2 grow tc bw2 shadow-5'>
+            <img alt='robots' width='200' src={`https://robohash.org/${id}?set=set4`} />
+            <div className='white'>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
+        </div>
+    );
+}
+
+export default Card;
