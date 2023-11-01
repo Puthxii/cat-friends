b50c850893749fa29a4891fda4210e90bd4a8649
Teenapat Rattanawong <puthxii@gmail.com>
2023-11-01 15:04:06 +0700

    Functional Component with Hooks

--- src/CardList.js
+++ src/CardList.js
@@ -1,50 +1,44 @@
-import React, {Component} from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
 import SearchBox from "./SearchBox";
 
-class CardList extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            cats: [],
-            searchField: ''
+const CardList = () => {
+    const [cats, setCats] = useState([]);
+    const [searchField, setSearchField] = useState('');
+
+    const filteredCats= cats.filter(cat => {
+        return cat.name.toLowerCase().includes(searchField.toLowerCase());
+    });
+
+    const onSearchChange = (event) => {
+      setSearchField(event.target.value);
+    };
+
+    useEffect(() => {
+        fetch(`https://jsonplaceholder.typicode.com/users`)
+          .then((results) => {
+              return results.json();
+          }).then(data => {
+            setCats(data);
+        });
+    },[]);
+
+    return (<div className='tc'>
+        <h1 className='f1'>Cat Friends</h1>
+        <SearchBox searchChange={onSearchChange}/>
+        {
+            filteredCats.map((user, i) => {
+                return (
+                  <Card
+                    key={filteredCats[i].id}
+                    id={filteredCats[i].id}
+                    name= {filteredCats[i].name}
+                    email={filteredCats[i].email}
+                  />
+                );
+            })
         }
-    }
-
-    onSearchChange = (event) => {
-        this.setState({searchField: event.target.value});
-    }
-
-    componentDidMount() {
-        fetch('https://jsonplaceholder.typicode.com/users')
-            .then(response => response.json())
-            .then(users => this.setState({cats: users}));
-    }
-
-    render() {
-        const {cats, searchField} = this.state;
-        const filteredCats = cats.filter(cat => {
-            return cat.name.toLowerCase().includes(searchField.toLowerCase());
-        })
-
-
-        return (<div className='tc'>
-            <h1 className='f1'>Cat Friends</h1>
-            <SearchBox searchChange={this.onSearchChange}/>
-            {
-                filteredCats.map((user, i) => {
-                    return (
-                        <Card
-                            key={filteredCats[i].id}
-                            id={filteredCats[i].id}
-                            name= {filteredCats[i].name}
-                            email={filteredCats[i].email}
-                        />
-                    );
-                })
-            }
-        </div>);
-    }
+    </div>);
 }
 
 export default CardList;
